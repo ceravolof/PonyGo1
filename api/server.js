b8a434d3e22a4bd9bca45cc2de5e21d9d8d49d14
ceravolof <133098730+ceravolof@users.noreply.github.com>
2024-10-22 17:13:48 +0000

    2

---
+++ api/server.js
@@ -0,0 +1,98 @@
+const express = require ('express');
+const sqlite3 = require ('sqlite3').verbose();
+const app = express();
+const port = 3000;
+
+let db = new sqlite3.Database('./database.db',(err)=>{
+    if(err){
+        return console.error(err.message);
+    }
+    console.log('Connesso al database SQLite');
+});
+
+app.use(express.json());
+
+db.run(`CREATE TABLEID NOT EXISTS utenti (
+    id PRIMARY KEY,
+    userid TEXT,
+    email TEXT,
+    passwd TEXT,
+    type TEXT,
+    )`);
+
+    app.post('/utenti',(req,res)=> {
+        const {userid,email,passwd,type} = req.body;
+        db.run(`INSERT INTO utenti (userid,email,passwd,type) VALUES (?,?,?,?) , ` [userid,email,passwd,type], (err)=>{
+            if (err){
+                return res.status(500).json({error:err.message});
+            }
+            res.json({message:'Nuovo utente creato',id: this.lastID});
+        });
+    });
+
+app.get('/utenti',(req,res)=>{
+    db.all('SELECT * FROM utenti',[], (err,rows)=>{
+        if(err){
+            return res.status(500).json({eror: err.message});
+        }
+        res.json({users:row});
+    });
+});
+
+app.put('/utenti/:id',(req,res)=> {
+    const {id} = req.params;
+    const {userid,email,passwd,type} =req.body;
+    db.run(
+        `UPDATE utenti SET userid = ?, email = ?,passwd = ? , type =?, WHERE id = ?`,
+        [userid,email,passwd,type,id],
+        function(err){
+            if(err){
+                return res.status(500).json({error: err.message});
+            }
+            if (this.changes === 0){
+                return res.status(404).json({message:'Utente non trovato'});
+            }
+            res.json({message: 'Utente aggiornato con successo'});
+        }
+    );
+});
+
+app.delete('/utenti:id', (req,res)=>{
+    const {id} = req.params;
+    db.run(`DELETE FROM utenti WHERE id = ?`,[id], function(err){
+        if (err){
+            return res.status(500).json({error: err.message});
+        }
+        if(this.changes ===0){
+            return res.status(404).json({message:'Utente non trovato'})
+        }
+        res.json({message: 'Utente cancellato con successo'})
+    });
+});
+
+/*
+app.get('/utenti/:city',(req,res)=> {
+
+    const citta = req.params.city;
+    db.all(`SELECT * FROM utenti WHERE citta = ?`,[citta], (err, rows)=> {
+        if(err){
+            return res.status(500).json({error: err.message});
+        }
+        res.json({users:rows});
+    });
+});
+*/
+
+process.on('SIGIT', () => {
+ db.close((err) => {
+    if(err){
+        console.error(err.message);
+    }
+    console.log('Chiusura del database.');
+    process.exit(0);
+ });
+});
+
+app.listen(port, () => {
+    console.log(`Server API in esecuzione su https://localhost:${port}`);
+});
